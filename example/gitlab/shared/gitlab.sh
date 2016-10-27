b218b2dd7b6c2601038035eff2a59fe6ca3e1b8a
Colin Huang <sidawei@gmail.com>
2016-10-27 15:08:39 +0800

    chore: update gitlab

--- example/gitlab/shared/gitlab.sh
+++ example/gitlab/shared/gitlab.sh
@@ -3,69 +3,13 @@ QPKG_CONF=/etc/config/qpkg.conf
 QPKG_NAME=gitlab
 QPKG_DISPLAY_NAME=$(/sbin/getcfg $QPKG_NAME Display_Name -f $QPKG_CONF)
 CONTAINER_STATION_DIR=$(/sbin/getcfg container-station Install_Path -f $QPKG_CONF)
-DOCKER=$CONTAINER_STATION_DIR/bin/system-docker
-JQ=$CONTAINER_STATION_DIR/usr/bin/jq
-QBUS=$CONTAINER_STATION_DIR/bin/qbus
-URI=com.qnap.dqpkg/qpkg/$QPKG_NAME
 
-
+# source qpkg/dqpkg functions
+QTS_LOG_TAG="$QPKG_DISPLAY_NAME"
 . $CONTAINER_STATION_DIR/script/qpkg-functions
+. $CONTAINER_STATION_DIR/script/dqpkg-functions
 
-
-wait_qcs_ready() {
-    if ! which ${DOCKER} >/dev/null 2>&1; then
-        # waiting for lxc
-        TIMEOUT=10
-        if qts_qpkg_is_enabled "container-station"; then
-            local count=0
-            while ! which lxc-start >/dev/null 2>&1; do
-                sleep 1
-                (( count++ ))
-                [ $count -ge $TIMEOUT ] && break
-            done
-        fi
-
-        if ! which ${DOCKER} >/dev/null 2>&1; then
-            local msg="Container Station is not installed or enabled."
-            if qts_find_parent_process $$ appRequest.cgi; then
-                qts_log_error "$msg"
-                qts_qpkg_disable $QPKG_NAME
-            else
-                echo "$msg"
-            fi
-            qts_error_exit
-        fi
-    fi
-}
-
-qbus_cmd() {
-    # $1: command {start|stop}
-    $QBUS put com.qnap.dqpkg/qpkg/${QPKG_NAME}/$1
-}
-
-complete_action() {
-    # $1: expected states
-    # $2: timeout
-    echo "Wait \"$1\" state in $2 seconds"
-    for ((i=0; i<=$2; i++)); do
-        echo -n "."
-        output=`QBUS_FORMAT=indent-json $QBUS get $URI`
-        code=`echo $output | $JQ .code`
-        state=`echo $output | $JQ .result.state | sed 's/\"//g'`
-        progress=`echo $output | $JQ .result.progress`
-        if [ "$code" != "200" ]; then
-            echo "ERROR: get error response code: $code"
-            break
-        fi
-        if echo "$1" | grep -q "$state"; then
-            break
-        fi
-        sleep 1; 
-    done
-    echo
-}
-
-
+# main
 case "$1" in
   start)
     if ! qts_qpkg_is_enabled $QPKG_NAME; then
@@ -78,7 +22,7 @@ case "$1" in
 
   stop)
     qbus_cmd stop
-    complete_action "stopped" 30
+    complete_action "removed stopped" 30
     ;;
 
   restart)
